d6d2d198600b9d64d5cb492b8a0f5487aa9b1e31
Ronald (Jeff) Lopez <32562609+lopezjronald@users.noreply.github.com>
2020-05-03 20:43:39 -0700

    Update Film.java

--- Film.java
+++ Film.java
@@ -18,25 +18,25 @@ public class Film {
 	public Film() throws ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver");
 	}
-
-	public void showFilmInfoUsingKeyword(String keyword) {
+	
+	public void showFilmInfoByKeyword(String keyword) {
 		String user = "student";
 		String password = "student";
 		String sqlText;
-
+		String filmId = null;
 		filmIdList.clear();
-		filmLanguageList.clear();
-		filmActorList.clear();
-
+		
 		sqlText = "SELECT film.id, film.title, film.release_year, film.rating, film.description, language.name , actor.first_name, actor.last_name "
-				+ "FROM film " + "JOIN language ON film.language_id = language_id "
-				+ "JOIN film_actor ON film_actor.film_id = film.id " + "JOIN actor ON actor.id = film_actor.actor_id "
+				+ "FROM film " 
+				+ "JOIN language ON film.language_id = language_id "
+				+ "JOIN film_actor ON film_actor.film_id = film.id " 
+				+ "JOIN actor ON actor.id = film_actor.actor_id "
 				+ "WHERE title LIKE \"%" + keyword + "%\";";
-
-		try (Connection connection = DriverManager.getConnection(URL, user, password);
-				PreparedStatement stmt = connection.prepareStatement(sqlText);
-				ResultSet rs = stmt.executeQuery();) {
-
+		
+		try ( Connection connection = DriverManager.getConnection(URL,user,password);
+		      PreparedStatement stmt = connection.prepareStatement(sqlText);
+		      ResultSet rs = stmt.executeQuery(); ) {
+			
 			boolean isNotEmpty = rs.next();
 
 			if (isNotEmpty) {
@@ -46,151 +46,149 @@ public class Film {
 			} else {
 				System.out.println("There is no film with that keyword.");
 			}
-
+			
 			while (rs.next()) {
+				
 				if (filmIdList.contains(rs.getString("film.id"))) {
 					continue;
+				} else {
+					System.out.println("Film ID: " + rs.getString("film.id") + ", Film Title: " + rs.getString("film.title")
+					+ ", Release Year: " + rs.getString("film.release_year") + "\nDescription: "
+					+ rs.getString("film.description"));
+					filmIdList.add(rs.getString("film.id"));
+					System.out.println("----------------------");
+					showFilmLanguages(rs.getString("film.id"));
+					System.out.println("----------------------");
+					showFilmActors(rs.getString("film.id"));
+					System.out.println("----------------------");
 				}
-				System.out.println("Film ID: " + rs.getString("film.id") + ", Film Title: " + rs.getString("film.title")
-						+ ", Release Year: " + rs.getString("film.release_year") + "\nDescription: "
-						+ rs.getString("film.description"));
-				filmIdList.add(rs.getString("film.id"));
+			}
+		} catch (SQLException e) {
+			//Ignore Exception
+		}
+}
+	
+	public void showFilmLanguages(String filmId) {
+		String user = "student";
+		String password = "student";
+		String sqlText;
+		filmLanguageList.clear();
+		
+		sqlText = "SELECT film.id, film.title, film.release_year, film.rating, film.description, language.name , actor.first_name, actor.last_name "
+				+ "FROM film " + "JOIN language ON film.language_id = language_id "
+				+ "JOIN film_actor ON film_actor.film_id = film.id " + "JOIN actor ON actor.id = film_actor.actor_id "
+				+ "WHERE film.id = " + filmId + ";";
+		
+		try ( Connection connection = DriverManager.getConnection(URL, user, password);
+			  PreparedStatement stmt = connection.prepareStatement(sqlText);
+			  ResultSet rs = stmt.executeQuery();) {
+
+			System.out.println("Langauge(s):");
+			
+			while (rs.next()) {
 				
-				
-				System.out.println("----------------------");
-				System.out.println("Languages:");
-				
-				while (rs.previous()) {
-					if (filmLanguageList.contains(rs.getString("language.name"))) {
-						continue;
-					}
+				if (filmLanguageList.contains(rs.getString("language.name"))) {
+					continue;
+				} else {
 					System.out.println(rs.getString("language.name"));
 					filmLanguageList.add(rs.getString("language.name"));
 				}
-				
-				
-				System.out.println("----------------------");
-				System.out.println("Actors:");
-				
-				while (rs.next()) {
-					if (filmActorList.contains(rs.getString("actor.first_name"))
-							&& filmActorList.contains(rs.getString("actor.last_name"))) {
-						continue;
-					}
-					System.out.println(rs.getString("actor.first_name") + " " + rs.getString("actor.last_name"));
-					filmActorList.add(rs.getString("actor.first_name"));
-					filmActorList.add(rs.getString("actor.last_name"));
-				}
 			}
-
-//			if (isNotEmpty) {
-//				System.out.println("----------------------");
-//				System.out.println("Languages:");
-//			}
-//
-//			while (rs.previous()) {
-//				if (filmLanguageList.contains(rs.getString("language.name"))) {
-//					continue;
-//				}
-//				System.out.println(rs.getString("language.name"));
-//				filmLanguageList.add(rs.getString("language.name"));
-//			}
-//
-//			if (isNotEmpty) {
-//				System.out.println("----------------------");
-//				System.out.println("Actors:");
-//			}
-//
-//			while (rs.next()) {
-//				if (filmActorList.contains(rs.getString("actor.first_name"))
-//						&& filmActorList.contains(rs.getString("actor.last_name"))) {
-//					continue;
-//				}
-//				System.out.println(rs.getString("actor.first_name") + " " + rs.getString("actor.last_name"));
-//				filmActorList.add(rs.getString("actor.first_name"));
-//				filmActorList.add(rs.getString("actor.last_name"));
-//			}
-//			System.out.println("----------------------");
 		} catch (SQLException e) {
-			System.err.println(e);
+			//Ignore Exception
 		}
-
 	}
+	
+	public void showFilmActors(String filmId) {
+		String user = "student";
+		String password = "student";
+		String sqlText;
 
-	public void showFilmInfoUsingID(String filmId) throws SQLException {
+		filmActorList.clear();
+		
+		sqlText = "SELECT film.id, film.title, film.release_year, film.rating, film.description, language.name , actor.first_name, actor.last_name "
+				+ "FROM film " + "JOIN language ON film.language_id = language_id "
+				+ "JOIN film_actor ON film_actor.film_id = film.id " + "JOIN actor ON actor.id = film_actor.actor_id "
+				+ "WHERE film.id = " + filmId + ";";
+		
+		try ( Connection connection = DriverManager.getConnection(URL,user,password);
+		      PreparedStatement stmt = connection.prepareStatement(sqlText);
+		      ResultSet rs = stmt.executeQuery(); ) {
+
+			System.out.println("Actor(s):");
+			
+			while (rs.next()) {
+				
+				if (filmActorList.contains(rs.getString("actor.first_name"))
+						&& filmActorList.contains(rs.getString("actor.last_name"))) {
+					continue;
+				}
+				System.out.println(rs.getString("actor.first_name") + " " + rs.getString("actor.last_name"));
+				filmActorList.add(rs.getString("actor.first_name"));
+				filmActorList.add(rs.getString("actor.last_name"));
+			}
+		} catch (SQLException e) {
+			//Ignore Exception
+		}
+	}
+	
+	public void showFilmInfoByFilmId(String filmId) {
 
 		String user = "student";
 		String password = "student";
 		String sqlText;
 		filmIdList.clear();
-		filmLanguageList.clear();
-		filmActorList.clear();
-
+		
 		sqlText = "SELECT film.id, film.title, film.release_year, film.rating, film.description, language.name , actor.first_name, actor.last_name "
 				+ "FROM film " + "JOIN language ON film.language_id = language_id "
 				+ "JOIN film_actor ON film_actor.film_id = film.id " + "JOIN actor ON actor.id = film_actor.actor_id "
 				+ "WHERE film.id = " + filmId + ";";
-
-		try (Connection connection = DriverManager.getConnection(URL, user, password);
-				PreparedStatement stmt = connection.prepareStatement(sqlText);
-				ResultSet rs = stmt.executeQuery();) {
-
+		
+		try ( Connection connection = DriverManager.getConnection(URL,user,password);
+		      PreparedStatement stmt = connection.prepareStatement(sqlText);
+		      ResultSet rs = stmt.executeQuery(); ) {
+			
 			boolean isNotEmpty = rs.next();
-
-			if (isNotEmpty) {
-
-			}
 
 			if (isNotEmpty) {
 				System.out.println("----------------------");
 				System.out.println("Film Information:");
 				rs.previous();
 			} else {
-				System.out.println("There is no film with that film ID.");
+				System.out.println("There is no film with that Film ID.");
 			}
-
+			
 			while (rs.next()) {
-				while (true) {
-					if (filmIdList.contains(rs.getString("film.id"))) {
-						// do nothing
-					} else {
-						System.out.println("Film ID: " + rs.getString("film.id") + ", Film Title: "
-								+ rs.getString("film.title") + ", Release Year: " + rs.getString("film.release_year")
-								+ "\nDescription: " + rs.getString("film.description"));
-						filmIdList.add(rs.getString("film.id"));
-					}
-
+				
+				if (filmIdList.contains(rs.getString("film.id"))) {
+					continue;
+				} else {
+					System.out.println("Film ID: " + rs.getString("film.id") + ", Film Title: " + rs.getString("film.title")
+					+ ", Release Year: " + rs.getString("film.release_year") + "\nDescription: "
+					+ rs.getString("film.description"));
+					filmIdList.add(rs.getString("film.id"));
 					System.out.println("----------------------");
-					System.out.println("Languages:");
-					while (rs.next()) {
-						if (filmLanguageList.contains(rs.getString("language.name"))) {
-							//do nothing
-						} else {
-							System.out.println(rs.getString("language.name"));
-							filmLanguageList.add(rs.getString("language.name"));
-						}
-					}
-
+					showFilmLanguages(rs.getString("film.id"));
 					System.out.println("----------------------");
-					System.out.println("Actors:");
-					while (rs.next()) {
-						if (filmActorList.contains(rs.getString("actor.first_name"))
-								&& filmActorList.contains(rs.getString("actor.last_name"))) {
-							continue;
-						} else {
-							System.out.println(rs.getString("actor.first_name") + " " + rs.getString("actor.last_name"));
-							filmActorList.add(rs.getString("actor.first_name"));
-							filmActorList.add(rs.getString("actor.last_name"));
-						}
-					}
-
+					showFilmActors(rs.getString("film.id"));
+					System.out.println("----------------------");
 				}
 			}
-
-			System.out.println("----------------------");
 		} catch (SQLException e) {
-			System.err.println(e);
+			//Ignore Exception
 		}
-
+		
+		
+		
 	}
+	
+	public void searchByKeyword(String keyword) throws SQLException {
+	showFilmInfoByKeyword(keyword);
 }
+	
+	public void searchByFilmId(String filmId) throws SQLException {
+		showFilmInfoByFilmId(filmId);
+	}
+
+}
+
